dd1790b7fad050f69b83e4958adfd85413abe118
Igor Dudek <dudekigor@gmail.com>
2020-08-06 09:01:29 +0200

    configuration eslint prettier husky + lint-stager with airbnb rules

--- src/App.js
+++ src/App.js
@@ -8,15 +8,25 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
+        asdasd asd Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores unde nemo
+        explicabo dolore, pariatur similique assumenda illum fugiat quos minus, magnam a officia
+        laboriosam cumque rerum quam natus et modi! Lorem ipsum dolor, sit amet consectetur
+        adipisicing elisas quae commodi aliquid dolor aliquam adipisci blanditiis! Laborum fugit ad
+        laboriosam quis eius eum repellat? Rem, enim. Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Blanditiis voluptatem sit ducimus aliquam fugit necessitatibus quibusdam
+        ea, voluptas impedit amet dolorum maxime eos? Dolorum architecto blanditiis maiores vero,
+        minima nemo.
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          react
         </a>
       </header>
     </div>
