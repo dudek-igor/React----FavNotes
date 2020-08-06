d2f48646afc1412f9fb261bfc4acc3bf5f7c3f22
Igor Dudek <dudekigor@gmail.com>
2020-08-06 10:31:33 +0200

    Clean project and add Styled-Components

---
+++ src/views/Root/Root.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Button from 'components/Button/Button';
+
+function Root() {
+  return (
+    <>
+      <h1>Hello Igor</h1>
+      <Button>My button </Button>
+    </>
+  );
+}
+
+export default Root;
