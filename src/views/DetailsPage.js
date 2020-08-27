c0b04261b64e33038ee3e3d5b45dbdfb2983d3ab
Igor Dudek <dudekigor@gmail.com>
2020-08-27 19:37:47 +0200

    Add function to show one note

--- src/views/DetailsPage.js
+++ src/views/DetailsPage.js
@@ -1,31 +1,58 @@
-import React from 'react';
+/* eslint-disable no-useless-catch */
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import DetailsTemplate from 'templates/DetailsTemplate';
 import { withContext } from 'hoc/withContext';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
-const DetailsPage = ({ activeItem }) => {
-  const [item] = activeItem;
+const DetailsPage = ({ activeItem, match }) => {
+  const [item, setItem] = useState();
+  useEffect(() => {
+    if (!activeItem) {
+      axios.get(`http://localhost:9000/api/note/${match.params.id}`).then(({ data }) => {
+        setItem(data);
+      });
+    } else {
+      const [data] = activeItem;
+      setItem(data);
+    }
+  }, [activeItem, match]);
+
   return (
-    <DetailsTemplate
-      title={item.title}
-      content={item.content}
-      articleUrl="/"
-      twitterName="/"
-      created={item.created}
-      key={item.id}
-      id={item.id}
-    />
+    <>
+      {item && (
+        <DetailsTemplate
+          title={item.title}
+          content={item.content}
+          articleUrl="/"
+          twitterName="/"
+          created={item.created}
+          key={item.id}
+          id={item.id}
+        />
+      )}
+    </>
   );
 };
 
 DetailsPage.propTypes = {
+  activeItem: PropTypes.oneOfType([PropTypes.string, PropTypes.array]).isRequired,
   // eslint-disable-next-line react/forbid-prop-types
-  activeItem: PropTypes.array.isRequired,
+  match: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  // eslint-disable-next-line no-underscore-dangle
-  activeItem: state[ownProps.pageContext].filter((item) => item._id === ownProps.match.params.id),
-});
+const mapStateToProps = (state, ownProps) => {
+  if (state[ownProps.pageContext]) {
+    return {
+      activeItem: state[ownProps.pageContext].filter(
+        // eslint-disable-next-line no-underscore-dangle
+        (item) => item._id === ownProps.match.params.id,
+      ),
+    };
+  }
+  return {
+    activeItem: '',
+  };
+};
 export default withContext(connect(mapStateToProps)(DetailsPage));
